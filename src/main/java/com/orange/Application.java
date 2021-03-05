0f96bcb546c55d3436e904376cb636c6c550a62d
orange <oltop@naver.com>
2021-03-05 18:06:28 +0900

    'first'

---
+++ src/main/java/com/orange/Application.java
@@ -0,0 +1,28 @@
+package com.orange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class); 
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+
+
